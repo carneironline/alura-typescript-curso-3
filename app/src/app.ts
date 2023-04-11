a224102ead15511beba81781c37ddf7287ed1f2d
Rodrigo Carneiro <carneironline@gmail.com>
2023-04-11 00:18:10 -0300

    first commit

---
+++ app/src/app.ts
@@ -0,0 +1,22 @@
+import { NegociacaoController } from './controllers/negociacao-controller.js';
+
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+const botaoImporta = document.querySelector('#botaoImporta');
+
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
+}
+
+if(botaoImporta) {
+    botaoImporta.addEventListener('click', () => {
+        controller.importaDados();
+    })
+} else {
+    throw Error('Botão importa não foi encontrado')
+}
